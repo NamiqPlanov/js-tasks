4fdc3b0c5680a3fdb0258ab40a73ee6c3a826444
NamiqPlanov245 <planovnamiq27@gmail.com>
2022-11-04 23:19:37 +0400

    changing structure of task3

--- task3.js
+++ task3.js
@@ -1,11 +1,11 @@
 let a = Number(prompt('Enter value: '));
 let b = prompt('(K)m or (M)ile: ')
 let c ;
-if (b = 'K') {
- c = a * 1.62;
+if (b == 'K') {
+ c = a * 0.62;
  alert ('You drive ' + c + ' miles in a day.');
 }
 else { 
-    c = a / 1.62;
+    c = a / 0.62;
     alert ('You drive ' + c + ' kms in a day.');
 }
