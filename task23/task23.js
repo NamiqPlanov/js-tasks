fbccf4da23398baa87a1bbc4e7b27365a227499e
NamiqPlanov245 <planovnamiq27@gmail.com>
2022-12-09 18:57:55 +0400

    new js tasks

--- task23/task23.js
+++ task23/task23.js
@@ -1,9 +1,11 @@
-<<<<<<< HEAD
+
 let abbreviation = function(name){
     let splitname = name.trim().split(' ')
     if(splitname.length>1){
         return (splitname[0]+' '+splitname[1].charAt(0)+'.')
-=======
+    }
+}
+
 // A: done ✅
 function abbreviation(name){
     split = prompt('Enter full name: ')
@@ -11,12 +13,12 @@ function abbreviation(name){
     
     if(split.length >1){
         console.log(split[0] + ' ' + split[1].charAt(0)  + '.')
->>>>>>> 80f799f716018540b54b04355358637b0e7fa4b5
+
     }
     return splitname[0]
 }
-<<<<<<< HEAD
+
 console.log(abbreviation('Namiq Planov'))
-=======
+
 abbreviation()
->>>>>>> 80f799f716018540b54b04355358637b0e7fa4b5
+
---
+++ task35/task35.js
@@ -0,0 +1,17 @@
+let elem1 = document.querySelector('body')
+let elem2 = document.querySelector('.div1')
+elem2.onclick =()=>{
+    elem1.style.backgroundColor = 'red'
+}
+let elem3 = document.querySelector('.div2')
+elem3.onclick = ()=>{
+    elem1.style.backgroundColor = 'green'
+}
+let elem4 = document.querySelector('.div3')
+elem4.onclick = ()=>{
+    elem1.style.backgroundColor = 'blue'
+}
+let elem5 = document.querySelector('.div4')
+elem5.onclick = ()=>{
+    elem1.style.backgroundColor = 'yellow'
+}
