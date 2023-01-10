85445ad324205fb55473a5c0cf15d99a04c2a146
NamiqPlanov245 <planovnamiq27@gmail.com>
2023-01-10 17:43:09 +0400

    new js tasks

---
+++ task49/task49.js
@@ -0,0 +1,9 @@
+async function getData(){
+    const value = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+    return value.data.meals[0]
+}
+async function writeData(){
+    const written = await getData()
+    console.log(written)
+}
+writeData()
---
+++ task50/task50.js
@@ -0,0 +1,29 @@
+let backend = {
+    'name':'Namiq',
+    'age':19
+}
+const value = new Promise((res,rej)=>{
+    setTimeout(()=>{
+        res(backend)
+    },500)
+    setTimeout(()=>{
+        rej('Duz ver backend datani')
+    },450)
+})
+/*value.then((info)=>{
+    console.log(info)
+}).catch((error)=>{
+    console.log(error)
+    console.log('alinmadi ey user')
+})*/
+
+async function getdata(){
+    try{
+        const info = await value
+        console.log(info)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+getdata()
